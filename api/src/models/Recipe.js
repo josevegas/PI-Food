d5098b30f96e34cbdc35e4c420222f660bd993a9
josevegas <36419038+josevegas@users.noreply.github.com>
2023-05-09 15:02:31 -0500

    avanzando la api

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -4,8 +4,35 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      isURL: true,
+    },
+    sumary: {
+      type: DataTransfer.STRING,
+      allowNull: false,
+    },
+    healthScore: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    stepToStep: {
+      type: DataTypes.ARRAY,
+      allowNull: false,
+    },
+    createdByDB: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
       allowNull: false,
     },
   });
