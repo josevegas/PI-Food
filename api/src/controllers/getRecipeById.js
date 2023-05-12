119ba5045fc9f4545db66c84f9761c676f4b9343
josevegas <36419038+josevegas@users.noreply.github.com>
2023-05-12 08:52:57 -0500

    avanzando rutas

--- api/src/controllers/getRecipeById.js
+++ api/src/controllers/getRecipeById.js
@@ -1,16 +1,23 @@
 const getAllRecipes=require('./getAllRecipes');
 require('dotenv').config();
 const {YOUR_API_KEY}= process.env;
-const axios=require('axios');
-const apiUrl=`https://api.spoonacular.com/recipes/${idRecipe}/information?apiKey=${YOUR_API_KEY}`;
+const axios=require('axios')
 
 const getRecipeById=async (req, res)=>{
     const {idRecipe}=req.params;
     if(idRecipe){
-        let recipeId= await axios.get(apiUrl);
-        recipeId.length?
-        res.status(200).json(recipeId):
-        res.status(404).json({message: 'Receta no encontrada'});
+        const apiUrl=await axios.get(`https://api.spoonacular.com/recipes/${idRecipe}/information?apiKey=${YOUR_API_KEY}`);
+        let infoId= await apiUrl.data;
+        const recipeId={
+            id: infoId.id,
+            name: infoId.title,
+            summary: infoId.summary,
+            healthScore: infoId.healthScore,
+            stepToStep: infoId.analyzedInstructions.steps,
+        }
+        //recipeId.length?
+        res.status(200).json(recipeId);
+        //res.status(404).json({message: `Receta ${idRecipe} no encontrada, ${recipeId.length}`});
     }
 }
 
