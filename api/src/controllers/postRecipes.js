e9fa94a5e8641c335acd2eaafa7d6a7cbc710727
josevegas <36419038+josevegas@users.noreply.github.com>
2023-05-19 23:00:11 -0500

    avanzando rutas

---
+++ api/src/controllers/postRecipes.js
@@ -0,0 +1,26 @@
+const{Recipe,Diets}=require('../db.js')
+
+const postRecipes= async (req,res)=>{
+    let{
+        name,
+        image,
+        summary,
+        healthScore,
+        stepToStep,
+        createByDb,
+        diets,
+    }=req.body;
+    let recipeCreate=await Recipe.create({
+        name,
+        image,
+        summary,
+        healthScore,
+        stepToStep,
+        createByDb,
+    })
+    let dietsDb= await Diets.findAll({where: {name:diets}});
+    recipeCreate.addDiets(dietsDb);
+    res.status(201).json({message:'Receta creada con éxito.'})
+};
+
+module.exports=postRecipes;
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,8 @@
 const { Router } = require('express');
 const getRecipeById=require('../controllers/getRecipeById.js');
 const getRecipeByName=require('../controllers/getRecipeByName.js');
-const getDiets=require('../controllers/getDiets.js')
+const getDiets=require('../controllers/getDiets.js');
+const postRecipes=require('../controllers/postRecipes.js');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -13,5 +14,6 @@ const router = Router();
 router.get('/recipes/:idRecipe',getRecipeById);
 router.get('/recipes',getRecipeByName);
 router.get('/diets',getDiets);
+router.post('/recipes',postRecipes);
 
 module.exports = router;
